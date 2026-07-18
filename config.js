f0216558484934b5f0c1631b649b956418083e46
S2525S <iigao2525@icloud.com>
2026-07-18 15:10:46 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-﻿window.MEATFLOW_CONFIG = {
+window.MEATFLOW_CONFIG = {
   LIFF_ID: '2010748894-FyJqsDsC',
-
-  // Apps Scriptを「ウェブアプリ」としてデプロイした /exec URLへ変更してください。
   GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbznT-2pOrmhApKVUVAqy6GR74TuM-8s0-Q7R42ft9gLIZn2aIjtEn4bqHQcmG8x1YNE/exec'
 };

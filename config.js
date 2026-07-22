0170ae105f2b6d0891253eb0796987b35e7cc398
S2525S <iigao2525@icloud.com>
2026-07-23 07:29:48 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MEATFLOW_CONFIG = {
-  LIFF_ID: '2010748894-FyJqsDsC',
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxXt9xVv6RaC3uataTl-mjAfg_d9L4Cem7gQIvJJ7vtUAiHIkYYN89rpQyd0cs5oumQ/exec'
+  LIFF_ID: '2010748894-FyJasDsC',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzC60PzMkIT5ALLVkgBRWGd0fHSsszoaRnNkiovEVUveS4IFXLae6RGSzleb-4qYIHgMQ/exec'
 };
